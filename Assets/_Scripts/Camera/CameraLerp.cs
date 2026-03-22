9a908caeae360a1b1dcacb18cebeea211988e67f
DESKTOP-OF6976T\Carlos <carlossanmiguelbns@gmail.com>
2026-03-22 21:10:13 +0100

    Decorations

--- Assets/_Scripts/Camera/CameraLerp.cs
+++ Assets/_Scripts/Camera/CameraLerp.cs
@@ -40,26 +40,26 @@ public class CameraLerp : MonoBehaviour
 
         if (trackedProyectile != null)
         {
-            newPos.y = Mathf.Clamp(trackedProyectile.transform.position.y, catapultPosition.position.y, castlePosition.position.y + 15);
+            newPos.y = Mathf.Clamp(trackedProyectile.transform.position.y, catapultPosition.position.y, castlePosition.position.y + 45);
             newPos.x = Mathf.Clamp(trackedProyectile.transform.position.x, catapultPosition.position.x, castlePosition.position.x);
 
-            Rigidbody2D proyectileRb = trackedProyectile.GetComponent<Rigidbody2D>();
-
             float speed = trackedRb.linearVelocity.magnitude;
-            float targetSize = Mathf.Clamp(5f + speed / 14f, 5f, 8f);
+            float targetFOV = Mathf.Clamp(60f + speed / 2f, 60f, 90f);
 
-            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetSize, Time.deltaTime * 5f);
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, targetFOV, Time.deltaTime * 5f);
         }
         else
         {
             trackedRb = null;
             newPos = new Vector3(catapultPosition.position.x, catapultPosition.position.y, transform.position.z);
             smoothSpeed = 2f;
-            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 5f, Time.deltaTime * 2f);
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 60f, Time.deltaTime * 2f);
         }
 
-        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * smoothSpeed);
-
-        
+        transform.position = new Vector3(
+            Mathf.Lerp(transform.position.x, newPos.x, Time.deltaTime * smoothSpeed),
+            Mathf.Lerp(transform.position.y, newPos.y, Time.deltaTime * smoothSpeed),
+            transform.position.z
+        );
     }
 }
